1c0cedcbca4c0d742a81dbb09688e00517175b7c
David Vogel <david.vogel@dyedurham.com>
2022-09-27 09:36:32 +1000

    moved panelMenu namespace into ui module

--- ui/index.d.ts
+++ ui/index.d.ts
@@ -1,8 +1,5 @@
 import St from '@gi-types/st1';
 import Clutter from '@gi-types/clutter10';
-import { panelMenu as PanelMenu } from './panelMenu';
-
-export class panelMenu {}
 
 export class PopupMenu {
   constructor(sourceActor: Clutter.Actor, arrowAlignment, arrowSide);
@@ -16,9 +13,20 @@ export class panel extends St.Widget {
   get boxOpacity(): number;
   set boxOpacity(value: number);
 
-  addToStatusArea(role: string, indicator: PanelMenu.Button, position: number, box: number): PanelMenu.Button;
+  addToStatusArea(role: string, indicator: panelMenu.Button, position: number, box: number): panelMenu.Button;
   toggleAppMenu(): void;
   toggleCalendar(): void;
   closeCalendar(): void;
   closeQuickSettings(): void;
 }
+
+export namespace panelMenu {
+  export class ButtonBox extends St.Widget {
+      parent: St.Widget;
+  }
+  export class Button extends ButtonBox {
+      public setSensitive(sensitive: boolean): void;
+      public setMenu(menu: PopupMenu): void;
+  }
+}
+export class panelMenu {}
