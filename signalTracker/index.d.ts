ec8267e16e40315032e933be6c18fdaf29a56a21
David Vogel <dvogel616@gmail.com>
2022-09-26 20:19:56 +1000

    initial commit

---
+++ signalTracker/index.d.ts
@@ -0,0 +1,28 @@
+import * as signals from "@gnome-shell-ts-mapping/signals";
+import * as GObject from "@gi-types/gobject2";
+
+
+export class TransientSignalHolder extends GObject.Object {
+    constructor(owner: signals.EventEmitter);
+    destroy(): void;
+}
+
+export class SignalManager {
+    constructor();
+    getDefault(): SignalManager;
+    getSignalTracker(obj: GObject.Object): SignalTracker;
+    maybeGetSignalTracker(obj: GObject.Object): SignalTracker;
+    removeSignalTracker(obj: GObject.Object): void;
+}
+
+export class SignalTracker {
+    constructor(owner: GObject.Object);
+    track(obj: GObject.Object, ...handlerIds: number[]): void;
+    untrack(obj: GObject.Object): void;
+    clear(): void;
+    destroy(): void;
+}
+
+export function connectObject(thisObj: GObject.Object, ...args: any[]): void;
+export function disconnectObject(thisObj: GObject.Object, obj: GObject.Object): void;
+export function registerDestroyableType(gtype: GObject.Type): void;
---
+++ ui/panelMenu/index.d.ts
@@ -0,0 +1,16 @@
+import St from "@gi-types/st1";
+import { PopupMenu } from "@gnome-shell-ts-mapping/ui";
+
+export namespace panelMenu {
+    export class ButtonBox extends St.Widget {
+        parent: St.Widget;
+    }
+    export class Button extends ButtonBox {
+        
+
+        public setSensitive(sensitive: boolean): void;
+        public setMenu(menu: PopupMenu): void;
+    }
+}
+
+export default panelMenu;
